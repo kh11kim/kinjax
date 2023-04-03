1f21af131e2c7a7dae9b5168228b99aeee2143f4
kh11kim <kanghyun777@naver.com>
2023-04-03 23:13:01 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+   
+setup(
+   name='kinjax',
+   version='0.0.2', 
+   description='This module calculates FK and Jacobian, accelerated by Jax to utilize JIT and GPU parallelization',
+   long_description    = long_description,
+   long_description_content_type='text/markdown',
+   author='Kanghyun Kim',
+   author_email='kh11kim@kaist.ac.kr',
+   packages=find_packages(),  #same as name
+   install_requires=[
+      "jax",
+      "sympy",
+      "scipy",
+   ]
+)
+
+# python setup.py sdist bdist_wheel
+# twine upload dist/*
